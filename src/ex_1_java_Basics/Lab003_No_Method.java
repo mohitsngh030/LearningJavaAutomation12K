7630147a274c7a15c09702bfcd1173bf7139b090
Mohit Singh <mohitsngh030@gmail.com>
2025-05-10 11:12:35 +0530

    First push

---
+++ src/ex_1_java_Basics/Lab003_No_Method.java
@@ -0,0 +1,12 @@
+package ex_1_java_Basics;
+
+public class Lab003_No_Method {
+    public static void main(String[] args) {
+        System.out.println("Comments");
+    }
+    /**
+     * dehwe
+     * kdwcnlc
+     *
+     */
+}
---
+++ src/ex_1_java_Basics/Multiple_Main.java
@@ -0,0 +1,15 @@
+package ex_1_java_Basics;
+
+public class Multiple_Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int args) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+}
